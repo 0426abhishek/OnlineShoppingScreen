0e654e216f123c75f35b53e1f39927c049d48f12
Abhishek <0426abhi@gmail.com>
2018-02-09 00:12:27 +0530

    Added Latest Code.

--- AmiTest/AppDelegate.swift
+++ AmiTest/AppDelegate.swift
@@ -16,6 +16,22 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+      
+        
+//        _ = Timer.scheduledTimer(withTimeInterval: 10, repeats: true) {
+//            (_) in
+//            print("Hello world")
+//            let sparklingHeart = "\u{1F496}"
+//            let alertView:UIAlertView = UIAlertView()
+//            alertView.title = "Abhishek weds garima!"
+//            alertView.message = "\(sparklingHeart)"
+//            alertView.delegate = self
+//            alertView.addButton(withTitle: "OK")
+//
+//            alertView.show()
+  //      }
+        
+        
         return true
     }
 
